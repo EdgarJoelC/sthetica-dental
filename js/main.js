607ab2590943173f3942df256a6db5a33973bc4b
EdDubu04 <eddubu04@gmail.com>
2025-12-26 21:55:57 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('active');
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
